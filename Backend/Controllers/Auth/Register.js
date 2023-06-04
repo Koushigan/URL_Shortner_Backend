ae6cc23f28c30ceb066ddbc3d03ee3700313e01f
Arthykarthik <93814265+Arthykarthik@users.noreply.github.com>
2023-06-04 13:40:56 +0530

    first commit

---
+++ Backend/Controllers/Auth/Register.js
@@ -0,0 +1,106 @@
+const User = require('../../Models/User')
+const crypto = require("crypto");
+const jwt = require('jsonwebtoken');
+const sendEmail = require('../../mailto/sendMail')
+
+exports.Register = async(req,res,next) => {
+    const {username,email,password} = req.body;
+    
+    try{
+        if(!email || !password || !username){
+            return res.status(400).json({
+                success : false,
+                error : "Please provide all the fields"
+            })
+        }
+        /* Check if user exists in DB */
+        const user = await User.findOne({email})
+        if(user){
+            return res.status(400).json({
+                success : false,
+                error: 'This email was already taken. Try another'
+            });
+        }
+
+        const accountActivationToken = jwt.sign(
+            {username,email,password},
+            process.env.Account_Activation_SECRET,
+            {expiresIn:process.env.Account_Activation_Token_EXPIRE}
+        )
+            
+        const accountActivateURL = `${process.env.Frontend_BaseURL}/Shortly/activateaccount/${accountActivationToken}`
+        const message = 
+        `
+        <p>Hi ${username},</p>
+
+        <p> Thanks for registration. Kindly follow below steps to activate your account </p>
+
+        <p> Click on the link to activate your account </p>
+
+        <a href=${accountActivateURL}>${accountActivateURL}</a>
+
+        <p>Link will be valid only for <strong>${process.env.Account_Activation_Token_EXPIRE}</strong></p>
+
+        <p>${process.env.EMAIL_OWNER}</p>
+        
+
+
+        <p>Thanks & Regards,</p>
+        PetiteURL Team
+        `
+        try{
+            await sendEmail({
+                to : email,
+                subject : process.env.EMAIL_SUBJECT,
+                text : message
+            })
+            return res.status(200).json({success:true,message : "Email Sent to activate your account. Make sure to check your spam."})
+        }catch(error){
+            return res.status(500).json({success:false,message : "Email couldn't be sent"})
+        }
+    }catch(error){
+        return res.status(500).send({
+            success : false,
+            error : error.message
+        })
+    }
+}
+
+exports.ActivateAccount = async (req,res,next) => {
+    const ActivateToken = req.params.activateToken;
+    /* Verify Token */
+    const {username,email,password} = jwt.verify(
+        ActivateToken,
+        process.env.Account_Activation_SECRET,
+        function(err, decoded) {
+            if (err) {
+                return res.status(401).json({
+                    success : false,
+                    error: 'Link has been expired. Signup again'
+                })
+            }else{
+                return decoded
+            }
+        }
+    )
+    try{
+        const user = await User.create({
+            username,
+            email,
+            password
+        })
+        const token = await user.getToken();
+        return res
+        .status(200)
+        .json({
+            success : true,
+            message : "Account Activated Successfully",
+            token
+        })
+    }catch(error){
+        return res.status(400).json({
+            success: false,
+            error: error.message
+        })
+    }
+}
---
+++ Backend/Controllers/Auth/ResetPassword.js
@@ -0,0 +1,38 @@
+const User = require('../../Models/User')
+const crypto = require('crypto')
+
+module.exports = ResetPassword = async (req,res,next) => {
+    const {password} = req.body;
+    if(!password){
+        return res.status(500).json({
+            success : false,
+            error : 'Please provide password'
+        })
+    }
+    const resetToken = crypto.createHash("kay555").update(req.params.resetToken).digest("zee")
+
+    try{
+        const user = await User.findOne({
+            resetPasswordToken : resetToken,
+            resetPasswordExpire: { $gt: Date.now() }
+        })
+        if(!user){
+            return res.status(400).json({
+                success : false,
+                error : "Password update failed"
+            })
+        }
+        user.password = req.body.password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+
+        await user.save();
+
+        return res.status(201).json({
+            success: true,
+            message: "Password Updated Success",
+        })
+    }catch(error){
+        return res.status(500).send(error)
+    }
+} 
